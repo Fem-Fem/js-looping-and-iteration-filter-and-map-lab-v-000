9473d04109bd1784098ec14908c1a03f6a3e5a70
Obafemi Fadairo <ofpupp@princeton.edu>
2018-06-18 00:57:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 }
 
 function exactMatch(drivers, object1) {
-  x = drivers.filter(function (driver) {
-    if (driver["revenue"] == Object.keys(object1)) {
-      return driver["revenue"]
-    }
-    if (driver["name"] == Object.keys(object1)) {
-      return driver["name"]
-    }
-  })
+  // x = drivers.filter(function (driver) {
+  //   if (driver["revenue"] == Object.keys(object1)) {
+  //     return driver["revenue"]
+  //   }
+  //   if (driver["name"] == Object.keys(object1)) {
+  //     return driver["name"]
+  //   }
+  // })
   return x;
 }
 
