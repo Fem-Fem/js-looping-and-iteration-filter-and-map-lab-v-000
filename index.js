747f3d12e614714c430c50b60ee2400fa074dc1c
Obafemi Fadairo <ofpupp@princeton.edu>
2018-06-18 02:33:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function driversWithRevenueOver(drivers, revenue) {
 function driverNamesWithRevenueOver(drivers, revenue) {
   x = drivers.filter(function (driver) {
     if (driver["revenue"] > revenue) {
+      debugger
       z = driver["name"]
       return z
     }
