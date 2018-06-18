a7a6bb44c244442bc48591742016f0d5178a953c
Obafemi Fadairo <ofpupp@princeton.edu>
2018-06-18 00:28:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  x = drivers.filter(function (driver) {
+    if (driver["revenue"] > revenue) {
+      newObj = {};
+      newObj["name"] = driver["name"]
+      newObj["name"] = revenue
+      return newObj;
+    }
+  })
+  return x;
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  x = drivers.filter(function (driver) {
+    if (driver["revenue"] > revenue) {
+      return `${driver.name}`;
+    }
+  })
+  return x;
+}
